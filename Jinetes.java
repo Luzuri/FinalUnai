dac94ff4cf3184a343aea9ffe9b09bba66b2dfae
otroadmin <otroadmin@DW1A-12.inform.local>
2019-05-31 08:49:35 +0200

    Jinetes
    Se añade la clase jinetes

---
+++ Jinetes.java
@@ -0,0 +1,37 @@
+
+public class Jinetes {
+
+	protected static class Muerte{String nombre = "Muerte";
+		   public Muerte(){super();};
+		   public String Invocar(){
+			   String string = "El fin se acerca, soy: ";
+			   return string;
+		   };
+		  }
+
+	protected static class Guerra{String nombre = "Guerra";
+		    public Guerra(){super();};
+		    public String Invocar(){
+		    	String string = "El fin se acerca, soy: ";
+		    	return string;}
+		  }
+
+	protected static class Peste{String nombre = "Peste";
+		    public Peste(){super();};
+		    public String Invocar(){
+		    	String string = "El fin se acerca, soy: ";
+		    	return string;}; 
+		  }
+
+	protected static class Hambre{String nombre = "Hambre";
+		    public Hambre(){super();};
+		    public String Invocar(){
+		    	String string = "El fin se acerca, soy: ";
+		    	return string;}; 
+		  }
+
+	public Jinetes() {
+		super();
+	}
+
+}
